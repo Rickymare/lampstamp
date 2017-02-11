2e012a1bf7f321429cb74d362e3bd8ec1ee7e514
Rickymare <rickypwnage@gmail.com>
2017-02-10 20:32:54 -0800

    Added stuff

--- data/data.sh
+++ data/data.sh
@@ -4,18 +4,22 @@ set -e
 set -u
 
 # Update the APT cache 
+apt update
 
 # Pre-seed debconf with MySQL server answers
 debconf-set-selections <<< "mysql-server mysql-server/root_password password bean"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password bean"
 
-# Install mySQL
+# Install mySql server
+apt install mysql-server
 
 # NOTE: potentially initialize database?
 
 # Secure installation
+mysql_secure_installation
 
 # Edit the bind address with ip address
+sed -i 's/bind-address.*/bind-address = 192.168.1.20' /etc/mysql/mysql.conf.d/mysql.cnf
 
 # Create usernames and passwords
 
