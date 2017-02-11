62b0046bf489a42ec27630be95d98223563c6f11
Ricky Sanchez <rickypwnage@gmail.com>
2017-02-10 20:04:02 -0800

    Added notes about scrypting

--- data/data.sh
+++ data/data.sh
@@ -2,3 +2,18 @@
 
 set -e
 set -u
+
+# Update the APT cache 
+
+# Install mySQL
+
+# NOTE: potentially initialize database?
+
+# Secure installation
+
+# Edit the bind address with ip address
+
+# Create usernames and passwords
+
+
+
