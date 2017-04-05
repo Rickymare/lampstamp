39b4206a25c0a751c83eea4ca8b3202a2fe7d640
Rickymare <rickypwnage@gmail.com>
2017-04-04 18:56:35 -0700

    added new lines of code

--- data/data.sh
+++ data/data.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
 
 set -e
 set -u
 
 # Update the APT cache 
 apt update
+
+# Install Apache2
+sudo apt install apache2
 
 # Pre-seed debconf with MySQL server answers
 debconf-set-selections <<< "mysql-server mysql-server/root_password password bean"
@@ -19,9 +22,25 @@ apt install mysql-server
 mysql_secure_installation
 
 # Edit the bind address with ip address
-sed -i 's/bind-address.*/bind-address = 192.168.1.20' /etc/mysql/mysql.conf.d/mysql.cnf
+sed -i 's/bind-address.*/bind-address = 97.120.234.128' /etc/mysql/mysql.conf.d/mysql.cnf
 
 # Create usernames and passwords
+
+# Install PHP and its dependencies
+sudo apt install php libapache2-mod-php php-mcrypt php-mysql
+
+# Open dir.conf and prioritize index.php
+echo "
+<IfModule mod_dir.c>
+    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
+</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
+
+# Restart the Apache2 server
+sudo systemctl restart apache2
+
+# Testing PHP on the server
+echo "<?php
+phpinfo();
 
 
 
