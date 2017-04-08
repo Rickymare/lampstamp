eb4c32a73b0ceecdb05dd2f7b1782d7abbffe253
Rickymare <rickypwnage@gmail.com>
2017-04-07 21:57:26 -0700

    Corrected sed syntax

--- data/data.sh
+++ data/data.sh
@@ -1,39 +1,37 @@
-#!/usr/bin/env bash
-
-set -e
-set -u
-
-# Update the APT cache 
-apt update
-
-# Pre-seed debconf with MySQL server answers PASSWORD DID NOT WORK
-export DEBIAN_FRONTEND="noninteractive" 
-debconf-set-selections <<< "mysql-server mysql-server/root_password password bean"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password bean"
-
-# Install mySql server
-apt install mysql-server
-
-# Edit the bind address with ip address
-sed -i 's/bind-address.*/bind-address = 97.120.234.128' /etc/mysql/mysql.conf.d/mysql.cnf
-
-# Create usernames and passwords
-
-# Install PHP and its dependencies
-sudo apt install php libapache2-mod-php php-mcrypt php-mysql
-
-# Open dir.conf and prioritize index.php
-echo "
-<IfModule mod_dir.c>
-    DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
-</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
-
-# Restart the Apache2 server
-sudo systemctl restart apache2
-
-# Creating a file to test PHP on the server
-touch /var/www/html/info.php
-
-echo "<?php
-phpinfo();
-?>" >> /var/www/html/info.php
+  1 #!/usr/bin/env bash
+  2 
+  3 set -e
+  4 set -u
+  5 
+  6 # Update the APT cache 
+  7 apt update -y
+  8 
+  9 # Pre-seed debconf with MySQL server answers
+ 10 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+ 11 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+ 12 
+ 13 # Install mySql server
+ 14 sudo apt -y install mysql-server
+ 15 
+ 16 # Edit the bind address with ip address
+ 17 sed -i 's/bind-address/bind-address = 10.154.31.192/' /etc/mysql/mysql.conf.d/mysqld.cnf
+ 18 
+ 21 # Install PHP and its dependencies
+ 22 sudo apt -y install php libapache2-mod-php php-mcrypt php-mysql
+ 23 
+ 24 # Edit dir.conf and prioritize index.php
+ 25 echo "
+ 26 <IfModule mod_dir.c>
+ 27     DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
+ 28 </IfModule>" >> /etc/apache2/mods-enabled/dir.conf
+ 29 
+ 30 # Restart the Apache2 server
+ 31 sudo systemctl restart apache2
+ 32 
+ 33 # Creating a file to test PHP on the server
+ 34 touch /var/www/html/info.php
+ 35 
+ 36 echo "<?php
+ 37 phpinfo();
+ 38 ?>" >> /var/www/html/info.php
+~                                      
--- remove/remove.sh
+++ remove/remove.sh
@@ -2,8 +2,9 @@
 
 # Run this script if data.sh or web.sh was unsuccessful. This script will remove the files that were added so we can try again with a fresh install.
 
-# Check if apache2 was installed, if so remove it
-if ! hash <apache2>
-  echo "it dont exist"
+# remove apache2
+sudo apt remove apache2
 
+# Remove MySql-server
+sudo apt remove mysql-server
 
