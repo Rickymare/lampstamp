86c1f9de9a01f44246c6ad41677ed34f4b9ac00f
rickymare <rickypwnage@gmail.com>
2017-04-17 22:13:25 -0700

    Removed restart apache2 to end of script

--- web/web.sh
+++ web/web.sh
@@ -12,6 +12,9 @@ sudo apt upgrade -y
 # Install Apache2
 sudo apt install apache2 -y
 
+# Install PHP and its dependencies
+sudo apt install php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-mysql php7.0-gd php-ssh2 -y
+
 # Adding ServerName and IP
 echo "ServerName 192.168.56.10" >> /etc/apache2/apache2.conf
 
@@ -21,12 +24,6 @@ echo "
      DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
 </IfModule>" > /etc/apache2/mods-enabled/dir.conf
 
-# Restarting apache2
-sudo systemctl restart apache2
-
-# Install PHP and its dependencies
-sudo apt install php7.0 libapache2-mod-php7.0 php7.0-mcrypt php7.0-mysql php7.0-gd php-ssh2 -y
-  
 # Creating a file to test PHP on the server
 echo "<?php
 phpinfo();
@@ -48,7 +45,10 @@ sed -i "/DB_USER/c\define('DB_USER', 'wordpressuser');" /wordpress/wp-config.php
 
 sed -i "/DB_PASSWORD/c\define('DB_PASSWORD', 'password');" /wordpress/wp-config.php
 
-sed -i "/DB_HOST/c\define('DB_HOST', '192.168.56.20');" /wordpress/wp-config.php
+sed -i "/DB_HOST/c\define('DB_HOST', '192.168.56.10');" /wordpress/wp-config.php
 
 # Copy WordPress folder into document root
-rsync -avP /wordpress/ /var/www/html/
+sudo rsync -avP /wordpress/ /var/www/html/
+
+# Restarting apache2
+sudo systemctl restart apache2
