889776def34086654230269bda4a6dc98cf4f652
rickymare <rickypwnage@gmail.com>
2017-04-18 21:53:28 -0700

    Cleaned up the casing and removed sudo since the script is running as root

--- data/data.sh
+++ data/data.sh
@@ -7,24 +7,23 @@ set -u
 apt update -y
 
 # Upgrade
-sudo apt upgrade -y
+apt upgrade -y
  
 # Pre-seed debconf with MySQL server answers
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
  
-# Install mySql server
-sudo apt install mysql-server -y
+# Install MySQL server
+apt install mysql-server -y
  
 # Edit the bind address with data server ip address
 sed -i "/bind-address/c\bind-address = 192.168.56.20" /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# Create Database, User, password, and grant privileges in MySQL
+# Create Database, User, Password, and grant privileges in MySQL
 mysql -uroot -ppassword -e "CREATE DATABASE wordpress;"
 mysql -uroot -ppassword -e "CREATE USER 'wordpressuser'@'192.168.56.10' IDENTIFIED BY 'password';"
 mysql -uroot -ppassword -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'192.168.56.10';"
 mysql -uroot -ppassword -e "FLUSH PRIVILEGES;"
-mysql -uroot -ppassword -e "exit"
 
 # Restart MySQL
 systemctl restart mysql.service
