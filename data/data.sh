f6619df6ba5fc6d59043f29fe4d6a3ebed1101e8
rickymare <rickypwnage@gmail.com>
2017-04-18 21:04:22 -0700

    Corrected mysql syntax, bind address, and added mysql restart

--- data/data.sh
+++ data/data.sh
@@ -17,13 +17,16 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again p
 sudo apt install mysql-server -y
  
 # Edit the bind address with data server ip address
-sed -i "/bind-address/c\bind-address = 192.168.56.10/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "/bind-address/c\bind-address = 192.168.56.20" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 # Create Database, User, password, and grant privileges in MySQL
 mysql -uroot -ppassword -e "CREATE DATABASE wordpress;"
-mysql -uroot -ppassword -e "CREATE USER wordpressuser@192.168.56.10 IDENTIFIED BY 'password';"
-mysql -uroot -ppassword -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@192.168.56.10;"
+mysql -uroot -ppassword -e "CREATE USER 'wordpressuser'@'192.168.56.10' IDENTIFIED BY 'password';"
+mysql -uroot -ppassword -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'192.168.56.10';"
 mysql -uroot -ppassword -e "FLUSH PRIVILEGES;"
 mysql -uroot -ppassword -e "exit"
+
+# Restart MySQL
+systemctl restart mysql.service
 
 echo "hey! I am at the end of the script"
